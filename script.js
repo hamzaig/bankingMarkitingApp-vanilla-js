d62bc31339b4402ecf65e15c1fbb2d4fb9ce4a1d
hamzaig <hamzaig@yahoo.com>
2021-08-29 20:36:34 +0500

    adding navigation smooth scrolling

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
 
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+let header = document.querySelector(".header");
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -31,8 +34,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//Cookie Bar /////////////////////////////////
 
-let header = document.querySelector(".header");
 const div = document.createElement("div");
 div.classList.add("cookie-message");
 div.innerHTML = "We use Cookies for good functionalilty and improved results... <button class='btn btn--close-cookie'>Got it!</button>"
@@ -48,9 +51,32 @@ div.style.width = "120%";
 
 div.style.height = Number.parseFloat(getComputedStyle(div).height) + 20 + "px";
 
+//Button Scrolling // learn more Button 
 
-const btnScrollTo = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
+btnScrollTo.addEventListener("click", (e) => {
+  section1.scrollIntoView({ behavior: "smooth" })
+})
 
-console.log(btnScrollTo, section1);
+// Page Navigation
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    let id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+})
+
+
+////
+const h1 = document.querySelector("h1");
+
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = "orangered"
+// h1.lastElementChild.style.color = "blue"
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
