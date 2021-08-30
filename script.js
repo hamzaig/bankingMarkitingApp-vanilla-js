37e4ce4a98b739c7b0fe522bec329c9f3276b3e8
hamzaig <hamzaig@yahoo.com>
2021-08-30 17:12:36 +0500

    implementation of sticky navbar with IntersectionObserver

--- script.js
+++ script.js
@@ -105,3 +105,24 @@ function menuFade(e) {
 
 nav.addEventListener("mouseover", menuFade.bind(0.5))
 nav.addEventListener("mouseout", menuFade.bind(1))
+
+// Sticky Navbar
+const headerForNav = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(headerForNav);
